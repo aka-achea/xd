b01f7b938356542f5ce76951adfb998f8a52dfba
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-03-07 16:45:55 +0800

    'update'

--- ed_ana.py
+++ ed_ana.py
@@ -7,31 +7,27 @@ from urllib.request import urlopen,Request,HTTPError,unquote
 from html.parser import HTMLParser
 import re
 
+
+
 # customized module
 from mylog import get_funcname,mylogger
 from sharemod import modstr,logfile,logfilelevel
-from openlink import op_simple
+from openlink import op_simple , op_requests
 
 def ana_song(weblink):
     l = mylogger(logfile,logfilelevel,get_funcname()) 
     # html = op_simple(weblink)[0]
-    # bsObj = BeautifulSoup(html,"html.parser")
+    html = op_requests(url,verify=False) 
+    bsObj = BeautifulSoup(html,"html.parser")
     # l.debug(bsObj)
 
-    from openlink import op_requests
-    content = op_requests(weblink).content
+    song_name = bsObj.find('em',{'class':'f-ff2'})
+    song_name = modstr(song_name.text)
+    l.info(song_name)
 
-    with open(r'E:\Music\t.html','wb') as f:
-        f.write(content)
-        f.close()
-
-    # song_name = bsObj.find('em',{'class':'f-ff2'})
-    # # song_name = modstr(song_name.text)
-    # l.debug(song_name)
-
-    # artist_name = bsObj.find('a',{'class':'s-fc7'})
-    # # artist_name = modstr(artist_name.text.strip())
-    # l.debug(artist_name)
+    artist_name = bsObj.find('a',{'class':'s-fc7'})
+    artist_name = modstr(artist_name.text.strip())
+    l.info(artist_name)
 
 def ana_cd(weblink):
     l = mylogger(logfile,logfilelevel,get_funcname()) 

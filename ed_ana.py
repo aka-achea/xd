6b9864b14e80f470675db0f6b2f5f1c6e39195d0
aka-achea <34272495+aka-achea@users.noreply.github.com>
2019-03-25 16:46:13 +0800

    'update'

--- ed_ana.py
+++ ed_ana.py
@@ -6,49 +6,68 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen,Request,HTTPError,unquote
 from html.parser import HTMLParser
 import re
-
+# from urllib.parse import urlparse
 
 
 # customized module
 from mylog import get_funcname,mylogger
-from sharemod import modstr,logfile,logfilelevel
+from sharemod import modstr,logfile
 from openlink import op_simple , op_requests
 
 def ana_song(weblink):
-    l = mylogger(logfile,logfilelevel,get_funcname()) 
-    html = op_simple(weblink)[0]
-    # html = op_requests(url,verify=False) 
+    ml = mylogger(logfile,get_funcname()) 
+    # html = op_simple(weblink)[0]
+    html = op_requests(url,verify=False).content 
     bsObj = BeautifulSoup(html,"html.parser")
-    # l.debug(bsObj)
+    # ml.debug(bsObj)
     # title = bsObj.find('title')
     # print(title)
 
     song_name = bsObj.find('em',{'class':'f-ff2'})
     songname = modstr(song_name.text.strip())
-    l.info(songname)
+    ml.info(songname)
     aa = bsObj.findAll('p',{'class':'des s-fc4'})
     artistname = modstr(aa[0].span.a.text)
     albumname = modstr(aa[1].a.text)
-    l.info(artistname)
-    l.info(albumname)
+    ml.info(artistname)
+    ml.info(albumname)
 
     cover = bsObj.find('div',{'class':'u-cover u-cover-6 f-fl'})
     cover = cover.img.attrs['href']
-    l.info(cover)
+    ml.info(cover)
     
     songmid = weblink.split('=')[-1]
 
     sDict = {'artist':artistname,'song_name':songname,'songmid':songmid,'cover':cover }
-    l.debug(sDict)
+    ml.debug(sDict)
     return sDict
 
 
 def ana_cd(weblink):
-    l = mylogger(logfile,logfilelevel,get_funcname()) 
+    ml = mylogger(logfile,get_funcname()) 
     html = op_simple(weblink)[0]
     bsObj = BeautifulSoup(html,"html.parser") #;print(bsObj)
 
+    albumname = bsObj.findAll('h2',{'class':'f-ff2'}).text
+    ml.info(albumname)
+
+    cover = bsObj.find('div',{'class':'cover u-cover u-cover-alb'})
+    cover = cover.img.attrs['href']
+    ml.info(cover)
+     
+    artistname = bsObj.find(text='歌手：')
+    artistname = artistname.next_siblings.a.text
+    ml.info(artistname)
+
+    year = bsObj.find(text='发行时间：').text
+    ml.info(year)
+
+
+
 
 if __name__ == "__main__":
-    url = 'https://music.163.com/#/song?id=1330348068'    
-    ana_song(url)
+    # url = 'https://music.163.com/#/song?id=1330348068'    
+    # sDict = ana_song(url)
+    
+    url = 'file:///E://1.html'
+    ana_cd(url)
--- ed_decry.py
+++ ed_decry.py
@@ -3,7 +3,9 @@
 # tested in win
 
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 import base64 , requests, sys
+import codecs
 
 headers = {
     'Cookie': 'appver=1.5.0.75771;',
@@ -15,13 +17,20 @@ first_param = "{\"ids\":\"[%d]\",\"br\":128000,\"csrf_token\":\"\"}"
 second_param = "010001"
 third_param = "00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7"
 forth_param = "0CoJUm6Qyw8W8jud"
+
+rankey = 16 * 'F'
 encSecKey = "257348aecb5e556c066de214e531faadd1c55d814f9be95fd06d6bff9f4c7a41f831f6394d5a3fd2e3881736d94a02ca919d952872e7d0a50ebfa1769a7a62d512f5f1ca21aec60bc3819a9c3ffca5eca9a0dba6d6f7249b06f5965ecfff3695b54e1c28f3f624750ed39e7de08fc8493242e26dbc4484a01c76f739e135637c"
 
 
-def get_params():
+def RSA_en(value,text,modulus): # not in use
+    text = text[::-1]
+    rs = int(codecs.encode(text.encode('utf8'),'hex_codec'),16) ** int(value,16) % int(modulus,16)
+    return format(rs,'x').zfill(256)
+
+def get_params(first_param):
     iv = "0102030405060708"
     first_key = forth_param
-    second_key = 16 * 'F'
+    second_key = 16 * 'F'  # as random str
     h_encText = AES_encrypt(first_param, first_key, iv)
     h_encText = AES_encrypt(h_encText, second_key, iv)
     return h_encText
@@ -42,32 +51,23 @@ def get_json(url, params, encSecKey):
         "params": params,
         "encSecKey": encSecKey
     }
-    response = requests.post(url, headers=headers, data=data).json()
-    print(response)
+    response = requests.post(url, headers=headers, data=data, verify=False).json()
+    # print(response)
     return response['data']
 
 def get_dlurl(id):
     first_param = "{\"ids\":\"[%d]\",\"br\":128000,\"csrf_token\":\"\"}" % int(id)
     url = 'https://music.163.com/weapi/song/enhance/player/url?csrf_token='
-    params = get_params()    
+    params = get_params(first_param)   
+    # encSecKey = RSA_en(second_param,rankey,third_param)  # not in use
     rsp = get_json(url, params, encSecKey)
     music_url = rsp[0].get('url')
     return music_url
 
 if __name__ == "__main__":
-    id = '1330348068'
-    first_param = "{\"ids\":\"[%d]\",\"br\":128000,\"csrf_token\":\"\"}" % int(id)
-    url = 'https://music.163.com/weapi/song/enhance/player/url?csrf_token='
-    params = get_params()
-    rsp = get_json(url, params, encSecKey)
-    music_url = rsp[0].get('url')
-
-    # music_url = get_dlurl(id)
+    id = '1352163999'
+    music_url = get_dlurl(id)
     print(music_url)
-    import myget
-    myget.dl(music_url)
 
-    # iv = "0102030405060708"
-    # key = "0CoJUm6Qyw8W8jud"
-    # encryptor = AES.new(key.encode("utf8"), AES.MODE_CBC,iv.encode("utf8"))
-    # print(encryptor)
+    # import myget
+    # myget.dl(music_url)

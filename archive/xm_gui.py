d8bf111c11095393f5b0e12f1c20f1add07cce89
aka-achea <cjyrb@hotmail.com>
2020-12-12 23:01:41 +0800

    'update'

--- archive/xm_gui.py
+++ archive/xm_gui.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 #coding:utf-8
 # test in Win
-# version:20190827
+__version__ = 20191208
 
 
 
-import os,sys,pathlib
+import os,sys
 import PySimpleGUI as sg      
 from multiprocessing import Process,freeze_support
-import pyautogui as auto
 
-from mytool import clickbutton,mywait,capture
-from xm_json import xm_json
+
 from config import logfile,dldir
+from xm_json import chromef12
 
 
 layout = [           
-    [sg.Text('Year',size=(5, 1)),sg.InputText(size=(20, 1),key='year')],    
+    [sg.Text('Year',size=(5, 1)),sg.InputText(size=(20, 1),key='year',default_text='2019')],    
     # [sg.Text('_'*36,justification='center')],          
     # [sg.Text('_'*36,justification='center')],
     [sg.Button('Go',tooltip='Click to start',key='go'),sg.Button('Stop',key='stop',disabled=True)]      
@@ -26,26 +25,15 @@ layout = [
 window = sg.Window('xiami',layout, grab_anywhere=False,size=(150,70))      
 
 
-def xmgui(workfolder):
+def xmgui(dldir):
     while True:
         event, values = window.Read()  
         if event is None:
             break        
         elif event == 'go':  
-            imgpath = os.path.join(pathlib.PurePath(__file__).parent,'img')
-            clickbutton( os.path.join(imgpath,'xm.png'))
-            auto.press('f12')
-            mywait(1)
-            auto.press('f5')
-            clickbutton( os.path.join(imgpath,'getalbumdetail.png'))
-            auto.click(button='right')
-            clickbutton( os.path.join(imgpath,'copy.png'))
-            clickbutton( os.path.join(imgpath,'copyresponse.png'))
             year = values['year']
-            xm_json(workfolder,year=year)
-            auto.hotkey('ctrl','w')
+            chromef12(year)
     window.Close()
-
 
 
 
@@ -53,8 +41,8 @@ def xmgui(workfolder):
 if __name__ == "__main__":
     freeze_support()
     try:
-        if os.path.exists(logfile):
-            os.remove(logfile)
+        # if os.path.exists(logfile):
+        #     os.remove(logfile)
         xmgui(dldir)
     except KeyboardInterrupt:
         print('ctrl + c')
--- archive/xm_json.py
+++ archive/xm_json.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 #coding:utf-8
 # tested in win
-#version:20190831
+__version__ = 20191126
 
 
 from urllib.request import unquote
 import json,os,re,shutil,random
+from pathlib import PurePath
 from pprint import pprint
 from bs4 import BeautifulSoup
+import pyautogui as auto
 
 # customized module
 from config import logfile,dldir
-import myget
+from openlink import op_simple,ran_header
 from mtag import addtag
 from mylog import get_funcname,mylogger
 from mp3archive import find_album,create_folder
-from mytool import mywait,get_text_clipboard
+from mytool import mywait,get_text_clipboard,clickbutton,capture
 from myfs import clean_f
 from myimg import squaresize
 from mystr import fnamechecker as modstr
-from openlink import op_simple,ran_header
+import myget
 
-
-headers = ran_header()
+ref = 'https://www.xiami.com/'
+headers = ran_header(ref=ref)
 
 
 def decry(code): 
@@ -60,7 +62,7 @@ def f2json(text):
         data = re.split('jsonp\d*',text)
         j = json.loads(data[1][1:-1])
     except TypeError as e:
-        ml.error(e)
+        ml.err(e)
         return None
     return j
 
@@ -70,6 +72,7 @@ def xm_json(workfolder,year=None,force=False):
     ml = mylogger(logfile,get_funcname()) 
 
     j = json.loads(get_text_clipboard())
+    print(j)
     # j = f2json(get_text_clipboard())
     j = j['result']['data']['albumDetail']
 
@@ -79,7 +82,7 @@ def xm_json(workfolder,year=None,force=False):
     album_name = modstr(j['albumName'])
     albumdir = f'{artist_name} - {year} - {album_name}'
     if find_album(albumdir) and force == False:
-        ml.warning(f'{albumdir} alread archived')
+        ml.warn(f'{albumdir} alread archived')
     else:
         albumfulldir = create_folder(workfolder,albumdir)
         try:
@@ -87,12 +90,12 @@ def xm_json(workfolder,year=None,force=False):
             cover = os.path.join(albumfulldir,albumdir+'.jpg')
             m_cover = os.path.join(albumfulldir,albumdir+'.png')
             if os.path.isfile(cover):
-                ml.warning('---- Big Cover download already !') 
+                ml.warn('---- Big Cover download already !') 
             else:
                 ml.info('Download big cover')
                 myget.dl(coverlink,out=cover)
             if os.path.isfile(m_cover):
-                ml.warning('---- Small cover ready !') 
+                ml.warn('---- Small cover ready !') 
             else:
                 shutil.copy(cover,m_cover)
                 squaresize(m_cover)
@@ -106,7 +109,7 @@ def xm_json(workfolder,year=None,force=False):
                 cdserial = str(j['songs'][s]['cdSerial'])
                 track = str(j['songs'][s]['track'])
                 if os.path.isfile(mp3):
-                    ml.warning(f'---- {songname} download already !') 
+                    ml.warn(f'---- {songname} download already !') 
                 else:
                     try:       
 
@@ -114,13 +117,14 @@ def xm_json(workfolder,year=None,force=False):
                         songid = j['songs'][s]['songId']
                         location = get_songlocation(songid) 
                         dlurl = decry(location)
+                        print(dlurl)
                         # dlurl = 'http:'+decry(location)
-                        myget.dl(dlurl,out=mp3) 
-
+                        # myget.dl(dlurl,out=mp3) 
+                        myget.simpledl(dlurl,mp3,verify=False,headers=headers)
                     except Exception as e :
-                        ml.error(e)
-                        ml.error("Content incomplete -> retry")
-                        myget.dl(dlurl,out=mp3) 
+                        ml.err(e)
+                        ml.err("Content incomplete -> retry")
+                        # myget.dl(dlurl,out=mp3) 
                         mywait(random.randint(1,3))
                 addtag(mp3,songname,album_name,artist_name,singers,
                         m_cover,year,track,cdserial) 
@@ -131,17 +135,46 @@ def xm_json(workfolder,year=None,force=False):
             pass
 
 
-if __name__ == "__main__":
-    import sys
-    try:
-        if sys.argv[1] == 'f':
-            force = True    
-    except IndexError:
-        force = False
-    if os.path.exists(logfile):
-        os.remove(logfile)
-    try:
-        xm_json(dldir,force=force)
-    except KeyboardInterrupt:
-        print('ctrl + c')
+def chromef12(year,autoclose=False):
+    """Chrome F12 find xhr and close page
+    ensure filter XHR"""
+    imgpath = os.path.join(PurePath(__file__).parent,'img')
+    if clickbutton( os.path.join(imgpath,'xm.png')):
+        auto.press('f12')
+    else:
+        raise
+    mywait(1)
+    auto.press('f5')
+    mywait(2)
+    if clickbutton( os.path.join(imgpath,'getalbumdetail.png')):
+        auto.click(button='right')
+        mywait(1)
+    else:
+        raise
+    if clickbutton( os.path.join(imgpath,'copy.png')):
+        mywait(1)
+    else:
+        raise    
+    if clickbutton( os.path.join(imgpath,'copyresponse.png')):
+        xm_json(dldir,year=year)
+    else:
+        raise
+    if autoclose:
+        auto.hotkey('ctrl','w')
 
+
+if __name__ == "__main__":
+    # import sys
+    # try:
+    #     if sys.argv[1] == 'f':
+    #         force = True    
+    # except IndexError:
+    #     force = False
+    # if os.path.exists(logfile):
+    #     os.remove(logfile)
+    # try:
+    #     xm_json(dldir,force=force)
+    # except KeyboardInterrupt:
+    #     print('ctrl + c')
+    id = '1768928710'
+    print(get_songlocation(id))
